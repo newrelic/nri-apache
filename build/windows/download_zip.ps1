f0b936ae7f18e2865d49916e9385c547723af9aa
Juan Manuel Perez <jperezflorez@newrelic.com>
2023-03-13 12:01:45 +0100

    first test of the refactor of the pipeline

--- build/windows/download_zip.ps1
+++ build/windows/download_zip.ps1
@@ -8,12 +8,9 @@ write-host "===> Creating dist folder"
 New-Item -ItemType directory -Path .\dist
 
 $VERSION=${TAG}.substring(1)
-$exe_folder="nri-${INTEGRATION}_windows_${ARCH}"
 $zip_name="nri-${INTEGRATION}-${ARCH}.${VERSION}.zip"
 
 $zip_url="https://github.com/${REPO_FULL_NAME}/releases/download/${TAG}/${zip_name}"
 write-host "===> Downloading & extracting .exe from ${zip_url}"
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 Invoke-WebRequest "${zip_url}" -OutFile ".\dist\${zip_name}"
-write-host "===> Expanding"
-expand-archive -path "dist\${zip_name}" -destinationpath "dist\${exe_folder}\"
